eb051874ac8c0da32bf358e25b626d702c909e48
Alejandro Fernandez <154221253+BanYio@users.noreply.github.com>
2025-06-27 18:41:11 +0200

    Add files via upload

---
+++ change-terminal-and-shortcuts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+REAL_USER=$(logname)
+QTERM_CONF="/home/$REAL_USER/.config/qterminal.org/qterminal.ini"
+
+echo -e "${GREEN}[+] Cambiando esquema de colores de QTerminal a Tango...${RESET}"
+
+# Reemplazar colorScheme=Kali-Dark por colorScheme=Tango
+sed -i 's/^colorScheme=Kali-Dark/colorScheme=Tango/' "$QTERM_CONF"
+echo -e "${GREEN}[+] Esquema de colores cambiado a Tango.${RESET}"
+
+# Cambiar el valor de KeyboardCursorShape de 0 a 2
+sed -i 's/^KeyboardCursorShape=[0-9]\+/KeyboardCursorShape=2/' "$QTERM_CONF"
+echo -e "${GREEN}[+] Forma del cursor del teclado cambiada.${RESET}"
+
+# Cambiar la transparencia de la terminal
+sed -i 's/^ApplicationTransparency=[0-9]\+/ApplicationTransparency=0/' "$QTERM_CONF"
+echo -e "${GREEN}[+] Transparencial de la terminal cambiada.${RESET}"
+
+# Cambiar la fuente de la terminal
+sed -i 's/^fontFamily=[^[:space:]]\+/fontFamily=Fira Code SemiBold/' "$QTERM_CONF"
+echo -e "${GREEN}[+] Fuente y tamaño cambiada.${RESET}"
+
+
+
+# Atajo de teclado para abrir clipmenu con CTRL+SHIFT+a
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Control><Shift>a" -n -t string -s "clipmenu"
+
+# Añadir atajo de teclado para abrir APPS con CTRL+SPACE
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary>space" -n -t string -s "xfce4-appfinder"
